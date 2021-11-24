6304905971b9e19e0fa5895df2654e0df728deb2
kmartinovic97 <94166244+kmartinovic97@users.noreply.github.com>
2021-11-24 14:50:13 +0100

    dodala random i popravila medijan

--- zad8.py
+++ zad8.py
@@ -7,19 +7,18 @@
 #		e) sve brojeve koji su manji od vrijednosti koju smo izračunali kao medijan navedene liste
 
 #	*Bonus: Napravite novu listu koja sadrži samo vrijednosti koje su za 10% veće ili manje od aritmetičke sredine
-import math
+import math, random
 lista = []
 lista_manji = []
 
-duljina_liste = int(input('Unesi duljinu liste: '))
+duljina_liste = random.randint(1,1000)
 
 #unos u listu
 for i in range(0, duljina_liste):
-    elem = int(input('Unesi element: '))
-    while elem not in range(0, 1001):
-        elem = int(input('Element ne zadovoljava uvijete, Unesi element: '))
-    lista.append(elem)
-print('lista = ', lista)
+    lista.append(random.randint(0,1000))
+print('lista = ', lista, "\n\n")
+
+ne_sortirana_lista = lista
 
 
 
@@ -27,22 +26,27 @@ print('lista = ', lista)
 #duljina neparna
 #indexi pocinju od 0, pa mi za centralni element  od duljine 5 treba 2 a ne 3
 if duljina_liste%2!=0:
+    lista = sorted(lista)
+    print('sortirana lista = ', lista, "\n\n")
     y = math.floor(duljina_liste/2)
     medijan = lista[y]
-    print('medijan je ', medijan)
+    print('medijan je ', medijan, "\n\n")
     for i in lista:
         if i < medijan:
             lista_manji.append(i)
 
 #parna duljina
 elif duljina_liste%2==0:
+    lista = sorted(lista)
+    print('sortirana lista = ', lista, "\n\n")
     x = int(duljina_liste/2)
     y = x - 1
     avg2 = ((lista[y]+lista[x])/2)
-    print('medijan je ', avg2)
+    print('medijan je ', avg2, "\n\n")
     for i in lista:
         if i < avg2:
             lista_manji.append(i)
+
 
 
 
@@ -57,7 +61,7 @@ def most_frequent(lista):
 
 
 
-print('modus je ', most_frequent(lista))
+print('modus je ', most_frequent(lista), "\n\n")
 
 
 #c) aritmetičku vrijednost
@@ -65,21 +69,16 @@ suma = 0
 for elem in lista:
     suma = suma + elem
 avg = suma/duljina_liste
-print('aritmeticka vrijednost je', avg)
+print('aritmeticka vrijednost je', avg, "\n\n")
 
 #d) sve brojeve koji se u definiranoj listi nalaze ispred vrijednosti koju smo izračunali kao medijan navedene liste
 
-print('brojevi koji se nalaze ispred medijana', lista[0:y])
 
-#sortiranje liste
-#lista = sorted(lista)
-#print(lista)
-#print(medijan)
+print('brojevi koji se nalaze ispred medijana', ne_sortirana_lista[0:y], "\n\n") #ne sortirana
+
 
 #e) sve brojeve koji su manji od vrijednosti koju smo izračunali kao medijan navedene liste
 
-
-
-
-print('brojevi manji od medijana', lista_manji)
+print('brojevi manji od medijana', lista_manji, "\n\n")
+print('test')
 
